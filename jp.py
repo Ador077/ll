dd3bf94a293269d8cf927e598e5759fcf603f6ef
Ado76 <119083581+Ador077@users.noreply.github.com>
2023-02-16 18:14:02 +0300

    Update jp.py

--- jp.py
+++ jp.py
@@ -54,8 +54,8 @@ def banner():
 	print("")
 	print("%s\033[1;32m╔══════════════════════════════════════════╗"%(Z))
 	print("\033[1;32m  ║\033[1;32m[•] Author\033[1;32m : ACTION-FIRE\033[1;32m║      
-	print("\033[1;91m  ║  Github\033[1;91m  : github.com/ACTION-FIRE\033[1;91m║\033[1;91m
-	print("\033[1;91m  ║  Facebook\033[1;91m: facebook.com/ft.alvi.18\033[1;91m║\033[1;91m
+	print("\033[1;91m  ║  Github\033[1;91m  : github.com/ACTION-FIRE\033[1;91m║
+	print("\033[1;91m  ║  Facebook\033[1;91m: facebook.com/ft.alvi.18\033[1;91m║
 	print("\033[1;91m  ║  Version\033[1;91m : V.3.0 \033[1;91m║\033[1;91m
 	print("033[1;32m   ╚══════════════════════════════════════════╝"%(Z))
 	print("")
